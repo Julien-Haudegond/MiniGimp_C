e65dcdf3a979b4d7f1490087acef4838afc3c7e7
MathildeDumoulin <mathildedumoulin54@gmail.com>
2019-01-16 23:52:45 +0100

    Arguments : intensité
    L'intensité peut être passée en argument (ex : DIMCON 50)

--- src/main.c
+++ src/main.c
@@ -15,25 +15,25 @@ int main(int argc, char *argv[])
 	if(argc >= 2){
 		Image I;
 		Image histoInitial;
-   		Image histoFinal;
-		//vérifier aussi que l'argument dans argv[1] est bien un fichier ppm
-		//LoadImage(&I,"images/PPM_Base/lake_P6.ppm");
+		Image histoFinal;
 		LoadImage(&I,argv[1]);
 		int displayHistogram = 0;
-
+		int intensity = 0;
    		for(int i = 2; i < argc; i++){
-   			
+   			if(i<argc-1){
+   				intensity = atoi(argv[i+1]);
+   			}
    			if(strcmp(argv[i], "ADDCON") == 0){
-   				ApplyLut(&I, 50, ADDCON);
-   				printf("Ajout de contraste \n");
+   				ApplyLut(&I, intensity, ADDCON);
+   				printf("Ajout de contraste de valeur %d \n",intensity);
    			}else if(strcmp(argv[i], "DIMCON") == 0){
-   				ApplyLut(&I, 50, DIMCON);
+   				ApplyLut(&I, intensity, DIMCON);
    				printf("Diminution du contraste \n");
    			}else if(strcmp(argv[i], "ADDLUM") == 0){
-   				ApplyLut(&I, 50, ADDLUM);
+   				ApplyLut(&I, intensity, ADDLUM);
    				printf("Ajout de luminosite \n");
    			}else if(strcmp(argv[i], "DIMLUM") == 0){
-   				ApplyLut(&I, 50, DIMLUM);
+   				ApplyLut(&I, intensity, DIMLUM);
    				printf("Diminution de la luminosite \n");
    			}else if(strcmp(argv[i], "INVERT") == 0){
    				printf("Inversion des couleurs \n");
@@ -42,19 +42,19 @@ int main(int argc, char *argv[])
    			}else if(strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "-histo") == 0){
    				displayHistogram = 1;
    				WriteHistogram(&I, &histoInitial);
-   				SaveImage(&histoInitial, "images/test_histo_initial.ppm");	
+   				SaveImage(&histoInitial, "images/test_histo_initial.ppm");
    			}
-   		}
-		if(displayHistogram == 1){
+			
+	   	}
+	   	if(displayHistogram == 1){
 			WriteHistogram(&I, &histoFinal);
 			SaveImage(&histoFinal, "images/test_histo_final.ppm");
 			FreeImage(&histoInitial);
-  			FreeImage(&histoFinal);
+	  		FreeImage(&histoFinal);
 		}
 		SaveImage(&I,"images/test.ppm");
-   		FreeImage(&I);
-   		
-   		return EXIT_SUCCESS;
+	   	FreeImage(&I);
+	   	return EXIT_SUCCESS;
 	}else{
 		printf("Missing argument : input file (filename.ppm)\n");
 		return EXIT_FAILURE;
@@ -62,7 +62,7 @@ int main(int argc, char *argv[])
 	
 }
 
-
+/*
 #ifdef DONTREADTHISCODE
 
 //MAIN DE TEST (pas définitif)
@@ -85,3 +85,4 @@ int main()
 }
 
 #endif
+*/
