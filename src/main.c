65ed9f436010384b73d242adb56f08330d8ad0b4
MathildeDumoulin <mathildedumoulin54@gmail.com>
2019-01-22 23:58:33 +0100

    Arguments #2
    Ajouts de messages d'erreurs pour les valeurs d'intensité (si nécessaire) + succès quand les fichiers ont été sauvegardés

--- src/main.c
+++ src/main.c
@@ -10,15 +10,14 @@
 #define MAX_FILENAME 70
 
 
-// ./bin/minigimp images/PPM_Base/lake_P6.ppm 
+// ./bin/minigimp images/PPM_Base/lake_P6.ppm
 const char *checkPPMFile(const char *ppmFile){
   //browses filename until it reaches the dot of the extensions
   const char *point = (strrchr(ppmFile,'.'));
   if(!point ||point == ppmFile){
-    printf("Unvalid filename for input PPM file \n");
     return "";
   }else{
-    printf(point+1);
+    //returns whats after the dot, if it's "ppm"
     return point + 1;
   }
 }
@@ -26,112 +25,133 @@ const char *checkPPMFile(const char *ppmFile){
 void displayMessage(int i){
   switch(i){
     case 1:
-    fprintf((stderr), "Unvalid filename for input PPM file \n"); break;
-    default : break;
+    fprintf((stderr), "Missing/Unvalid filename for input PPM file \n"); break;
+    case 2:
+    fprintf((stderr), "Unvalid filename for output PPM file \n"); break;
+    case 3:
+    fprintf((stderr),"Missing a value argument for a LUT. Value has been automatically set to 20 \n"); break;
+    case 4:
+    fprintf((stderr),"Missing/Unvalid option or name for output ppm file. Name has been automatically set to result.ppm \n"); break;
+    case 5:
+    fprintf((stderr),"PPM file saved successfully in folder ./images \n");break;
+    case 6:
+    fprintf((stderr),"Histogram saved successfully in folder ./images \n");break;
+    default : 
+    fprintf((stderr),"Unknown error\n");break;
   }
   
 }
 
 void displayExample(){
-  fprintf((stderr), "Try something like : ./bin/minigimp [PPM_IMAGE_NAME] [LUT_1] [VALUE_1] [LUT_N] [VALUE_N]");
-  fprintf((stderr), "LUTS that need an integer parameter [VALUE] : \n ADDCON \n DIMCON \n ADDLUM \n DIMLUM");
-  fprintf((stderr), "Following LUTS don't require any extra parameter : \n INVERT \n SEPIA \n RED \n GREEN \n BLUE");
+  fprintf((stderr), "Try something like : ./bin/minigimp PPM_IMAGE_NAME.ppm [-h] LUT_1 [VALUE_1] LUT_N [VALUE_N] [-o MYIMAGE.ppm] \n");
+  fprintf((stderr), "Value parameter is unnecessary for : INVERT, SEPIA, RED, GREEN, BLUE and DAYTON \n");
 }
 
   
 int main(int argc, char *argv[])
 {
-
-
-  //allocating memory for the string that will contain the path for the initial image
-  //int FilenameSize = 1+strlen(argv[1]);
-  //char *InitialPPMFile = (char *)malloc(sizeof(char)* FilenameSize);
-  char InitialImagePath[MAX_FILENAME]="./images/PPM_Base/";
-  //strcat(InitialPPMFile, argv[1]);
-  strcat(InitialImagePath,argv[1]);
-  //printf("%s",InitialImagePath);
-
-  
-  //printf("%s",FinalImagePath);
-
-  //char FinalImagePath[]="images/";
-
+  if(strcmp(checkPPMFile(argv[1]), "ppm") == 0){
+    char InitialImagePath[MAX_FILENAME]="./images/PPM_Base/";
+    strcat(InitialImagePath,argv[1]);
 	
-	if(argc >= 2){
-		Image I;
-		Image histoInitial;
-		Image histoFinal;
-    FinalLUT lutF;
-    initLutArray(&lutF);
-		LoadImage(&I,InitialImagePath);
+  	if(argc >= 2){
+  		Image I;
+  		Image histoInitial;
+  		Image histoFinal;
+      FinalLUT lutF;
+      initLutArray(&lutF);
+  		LoadImage(&I,InitialImagePath);
 
-    //initialing counters for optional arguments
-		int displayHistogram = 0;
-    int chooseFileName = 0;
-    
-		int intensity;
-   		for(int i = 2; i < argc; i++){
-        printf("%s \n", argv[i]);
-        intensity = 0;
-   			if(i<argc-1){
-   				intensity = atoi(argv[i+1]);
-   			}
-   			if(strcmp(argv[i], "ADDCON") == 0){
-   				selectLut(&lutF, intensity, ADDCON);
-   			}else if(strcmp(argv[i], "DIMCON") == 0){
-   				selectLut(&lutF, intensity, DIMCON);
-   			}else if(strcmp(argv[i], "ADDLUM") == 0){
-   				selectLut(&lutF, intensity, ADDLUM);
-   			}else if(strcmp(argv[i], "DIMLUM") == 0){
-   				selectLut(&lutF, intensity, DIMLUM);
-   			}else if(strcmp(argv[i], "INVERT") == 0){
-          selectLut(&lutF, intensity, INVERT);
-        }else if(strcmp(argv[i], "DAYTON") == 0){
-          selectLut(&lutF, intensity, DAYTON);
-   			}else if(strcmp(argv[i], "RED") == 0){
-          selectLut(&lutF, intensity, RED);
-        }else if(strcmp(argv[i], "GREEN") == 0){
-          selectLut(&lutF, intensity, GREEN);
-        }else if(strcmp(argv[i], "BLUE") == 0){
-          selectLut(&lutF, intensity, BLUE);
-        }else if(strcmp(argv[i], "SEPIA") == 0){
-          blackWhite(&I);
-          selectLut(&lutF, intensity, SEPIA);
-   			}else if(strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "-histo") == 0){
-          displayHistogram = 1;
-          WriteHistogram(&I, &histoInitial);
-          SaveImage(&histoInitial, "images/test_histo_initial.ppm");
-        }else if(strcmp(argv[i], "op") == 0){
-          chooseFileName = 1;
-          char FinalImagePath[MAX_FILENAME]="./images/";
-          strcat(FinalImagePath,argv[i+1]);
-          printf("%s \n",FinalImagePath);
-          ApplyLutToImage (&I, &lutF);
-          SaveImage(&I,FinalImagePath);
-   			}
-			
-	   	}
-    if(chooseFileName != 1){
-      ApplyLutToImage (&I, &lutF);
-    }
-	  if(displayHistogram == 1){
-			 WriteHistogram(&I, &histoFinal);
-			 SaveImage(&histoFinal, "images/test_histo_final.ppm");
-			 FreeImage(&histoInitial);
-	     FreeImage(&histoFinal);
-		}
-    if(chooseFileName != 1){
-      SaveImage(&I,"images/result.ppm");
-    }
-		  //free(InitialPPMFile);
-	   	FreeImage(&I);
-
-	   	return EXIT_SUCCESS;
-	}else{
-		printf("Missing argument : input file (filename.ppm)\n");
-		return EXIT_FAILURE;
-	}
+      //initialing counters for optional arguments
+  		int displayHistogram = 0;
+      int chooseFileName = 0;
+      int requiredIntensity = 0;
+      
+  		int intensity;
+     		for(int i = 2; i < argc; i++){
+          intensity = 0;
+          if(strcmp(argv[i], "ADDCON") == 0 || strcmp(argv[i], "DIMCON") == 0 || strcmp(argv[i], "ADDLUM") == 0 || strcmp(argv[i], "DIMLUM") == 0){
+            requiredIntensity = 1;
+          }
+     			if(i<argc-1 && requiredIntensity == 1){
+     				intensity = atoi(argv[i+1]);
+            //Missing a value after a LUT that is not the last argument
+            if(intensity == 0){
+              displayMessage(3);
+              intensity = 20;
+            }
+            printf("Intensity : %d \n", intensity);
+          //case where a LUT that is supposed to have an intensity value is the last argument
+     			}else if(i == argc-1 && requiredIntensity == 1){
+              displayMessage(3);
+              intensity = 20;
+          }
+          //Selecting the chosen LUTs
+     			if(strcmp(argv[i], "ADDCON") == 0){
+     				selectLut(&lutF, intensity, ADDCON);
+     			}else if(strcmp(argv[i], "DIMCON") == 0){
+     				selectLut(&lutF, intensity, DIMCON);
+     			}else if(strcmp(argv[i], "ADDLUM") == 0){
+     				selectLut(&lutF, intensity, ADDLUM);
+     			}else if(strcmp(argv[i], "DIMLUM") == 0){
+     				selectLut(&lutF, intensity, DIMLUM);
+     			}else if(strcmp(argv[i], "INVERT") == 0){
+            selectLut(&lutF, intensity, INVERT);
+          }else if(strcmp(argv[i], "DAYTON") == 0){
+            selectLut(&lutF, intensity, DAYTON);
+     			}else if(strcmp(argv[i], "RED") == 0){
+            selectLut(&lutF, intensity, RED);
+          }else if(strcmp(argv[i], "GREEN") == 0){
+            selectLut(&lutF, intensity, GREEN);
+          }else if(strcmp(argv[i], "BLUE") == 0){
+            selectLut(&lutF, intensity, BLUE);
+          }else if(strcmp(argv[i], "SEPIA") == 0){
+            blackWhite(&I);
+            selectLut(&lutF, intensity, SEPIA);
+     			}else if(strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "-histo") == 0){
+            displayHistogram = 1;
+            WriteHistogram(&I, &histoInitial);
+            SaveImage(&histoInitial, "images/histo_initial.ppm");
+            displayMessage(6);
+          }else if(strcmp(argv[i], "-o") == 0){
+            if(strcmp(checkPPMFile(argv[i+1]), "ppm") == 0){
+              chooseFileName = 1;
+              char FinalImagePath[MAX_FILENAME]="./images/";
+              strcat(FinalImagePath,argv[i+1]);
+              ApplyLutToImage (&I, &lutF);
+              SaveImage(&I,FinalImagePath);
+            }else{
+              displayMessage(2);
+              displayExample();
+              return EXIT_FAILURE;
+            }
+            
+     			}
+  			
+  	   	}
+      if(chooseFileName != 1){
+        ApplyLutToImage (&I, &lutF);
+      }
+  	  if(displayHistogram == 1){
+  			 WriteHistogram(&I, &histoFinal);
+  			 SaveImage(&histoFinal, "images/histo_final.ppm");
+  			 FreeImage(&histoInitial);
+  	     FreeImage(&histoFinal);
+  		}
+      if(chooseFileName != 1){
+        displayMessage(4);
+        SaveImage(&I,"images/result.ppm");
+      }
+  	   	FreeImage(&I);
+        displayMessage(5);
+  	   	return EXIT_SUCCESS;
+  	}
 	
+  }else{
+    displayMessage(1);
+    displayExample();
+    return EXIT_FAILURE;
+   }
 }
 
 
